7cfd2d5f068c5192eafb5b2a4ffb443163627c22
Jessé Levandovski <jesse.levandovski@outlook.com>
2021-03-01 20:35:21 -0300

    Upload do projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+     <app-root></app-root>
+     <script src="{{asset('js/runtime.js')}}" defer></script>
+     <script src="{{asset('js/polyfills.js')}}" defer></script>
+     <script src="{{asset('js/vendor.js')}}" defer></script>
+     <script src="{{asset('js/main.js')}}" defer></script>
+    </body>
+</html>
