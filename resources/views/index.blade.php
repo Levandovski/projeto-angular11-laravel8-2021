c3cff4de0ee1de16c7524d4f25e8339d4d790f3b
Jesse Levandovski <jessel@triadsystems.com.br>
2021-03-02 15:31:44 -0300

    Nova implementação de configurações do angular/material + front end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Projeto Galeria</title>
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <!--Import css-->
+        <link rel="stylesheet" href="{{asset('js/styles.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <!--Icons-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
      <app-root></app-root>
      <script src="{{asset('js/runtime.js')}}" defer></script>
